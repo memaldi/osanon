d8cdcf9af8a8e6cc7baf407aa6fe6cd0e2ee781b
Mikel Emaldi <m.emaldi@gmail.com>
2015-11-28 16:18:29 +0100

    Fixing bugs

--- wsgi/osanon/osanon/settings.py
+++ wsgi/osanon/osanon/settings.py
@@ -10,6 +10,7 @@ https://docs.djangoproject.com/en/1.8/ref/settings/
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 import os
+import urlparse
 DJ_PROJECT_DIR = os.path.dirname(__file__)
 BASE_DIR = os.path.dirname(DJ_PROJECT_DIR)
 WSGI_DIR = os.path.dirname(BASE_DIR)
